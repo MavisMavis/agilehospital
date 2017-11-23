e1b3227ffabd6f265158bedb6bd00058146c03f9
brandon9527 <brandon_lwc@hotmail.com>
2017-11-23 23:23:43 +0800

    sign in function

--- SignIn.php
+++ SignIn.php
@@ -1,10 +1,12 @@
 <?php
 	session_start();
-	if(isset($_SESSION['userSession'])!="")
-	{
-		header("Location: PendingApproval.php");
-	}
 	include_once 'DatabaseConnect.php';
+	
+	if(isset($_SESSION['userSession'])!=""){
+    	header("Location: DoctorView.php");
+    	exit;
+	}
+
 
 	if(isset($_POST['login']))
 	{
@@ -17,27 +19,7 @@
 		// If password match the database selected based on username
 		if(password_verify($password, $row['password'])){
 			$_SESSION['userSession'] = $row['id'];
-			header("Location: PendingApproval.php");
-		}
-		
-		if($row['level'] == 1)
-		{
-			header("Location: DoctorView.php"); 
-		}
-		
-		else if ($row['level'] == 2)
-		{
-			header("Location: NurseView.php");
-		}
-		
-		else if ($row['level'] == 3)
-		{
-			header("Location: admin.php");
-		}
-		
-		else if ($row['level'] > 3)
-		{
-			header ("Location:PendingApproval.php");
+			header("Location: DoctorView.php");
 		}
 		
 		else
@@ -63,10 +45,7 @@
 		<title>Hospital System - SignIn</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/main.css" />
-		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
 	</head>
 	<body>
 		<!-- Wrapper -->
@@ -112,7 +91,6 @@
 			<script src="assets/js/jquery.min.js"></script>
 			<script src="assets/js/skel.min.js"></script>
 			<script src="assets/js/util.js"></script>
-			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
 			<script src="assets/js/main.js"></script>
 
 	</body>
